ceb51f2e7e27dabc6242daab83fe7176983e1201
Leticia Speda <leticiaspeda@gmail.com>
2023-08-30 06:01:52 -0300

    feature: adicionado textos de email e senha com espaco para digitacao

--- TelaCadastro/TelaCadastro/RegisterViewController.swift
+++ TelaCadastro/TelaCadastro/RegisterViewController.swift
@@ -29,9 +29,40 @@ final class RegisterViewController: UIViewController {
         return label
     }()
     
+    private lazy var writePasswordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Digite sua senha:"
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     private lazy var nameTextField: UITextField = {
         let tf = UITextField()
         tf.placeholder = "ex: Joana"
+        tf.textColor = .gray
+        tf.layer.borderWidth = 1
+        tf.layer.borderColor = UIColor.gray.cgColor
+        tf.layer.cornerRadius = 8
+        tf.translatesAutoresizingMaskIntoConstraints =  false
+        return tf
+    }()
+    
+    private lazy var emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "ex: joana@gmail.com"
+        tf.textColor = .gray
+        tf.layer.borderWidth = 1
+        tf.layer.borderColor = UIColor.gray.cgColor
+        tf.layer.cornerRadius = 8
+        tf.translatesAutoresizingMaskIntoConstraints =  false
+        return tf
+    }()
+    
+    private lazy var passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "ex: 123@45"
         tf.textColor = .gray
         tf.layer.borderWidth = 1
         tf.layer.borderColor = UIColor.gray.cgColor
@@ -54,7 +85,12 @@ final class RegisterViewController: UIViewController {
     private func configureHierarchy() {
         view.addSubview(titleScreenLabel)
         view.addSubview(writeNameLabel)
+        view.addSubview(writeEmailLabel)
+        view.addSubview(writePasswordLabel)
         view.addSubview(nameTextField)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        
     }
     
     private func configureConstraints() {
@@ -69,17 +105,85 @@ final class RegisterViewController: UIViewController {
                 equalTo: titleScreenLabel.bottomAnchor,
                 constant: 14
             ),
-            writeNameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            writeNameLabel.leadingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                constant: 20
+            ),
+            
+            writeEmailLabel.topAnchor.constraint(
+                equalTo: nameTextField.bottomAnchor,
+                constant: 14
+            ),
+            writeEmailLabel.leadingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                constant: 20),
+            
+            writeEmailLabel.trailingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                constant: -20
+            ),
+            
+            writePasswordLabel.topAnchor.constraint(
+                equalTo: emailTextField.bottomAnchor,
+                constant: 14
+            ),
+            writePasswordLabel.leadingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                constant: 20),
+            
+            writePasswordLabel.trailingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                constant: -20
+            ),
             
             nameTextField.topAnchor.constraint(
                 equalTo: writeNameLabel.bottomAnchor,
                 constant: 14
             ),
-            nameTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            nameTextField.leadingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                constant: 20
+            ),
             
-            nameTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            nameTextField.trailingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                constant: -20
+            ),
             
-            nameTextField.heightAnchor.constraint(equalToConstant: 30)
+            nameTextField.heightAnchor.constraint(equalToConstant: 30),
+            
+            emailTextField.topAnchor.constraint(
+                equalTo: writeEmailLabel.bottomAnchor,
+                constant: 14
+            ),
+            emailTextField.leadingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                constant: 20
+            ),
+            
+            emailTextField.trailingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                constant: -20
+            ),
+            
+            emailTextField.heightAnchor.constraint(equalToConstant: 30),
+            
+            passwordTextField.topAnchor.constraint(
+                equalTo: writePasswordLabel.bottomAnchor,
+                constant: 14
+            ),
+            passwordTextField.leadingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                constant: 20
+            ),
+            
+            passwordTextField.trailingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                constant: -20
+            ),
+            
+            passwordTextField.heightAnchor.constraint(equalToConstant: 30),
+            
         ])
     }
     
